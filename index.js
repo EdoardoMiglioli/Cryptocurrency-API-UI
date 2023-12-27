481db780bae8b5ac987d16ca6f2e85277543822c
Edoardo Miglioli <edoardomiglioli@MacBook-Air-di-Edoardo.local>
2023-12-27 21:06:11 +0100

    Add main functionality of retriving data of a given crypto currency

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+const API_URL = "https://api.blockchain.com/v3/exchange/tickers/";
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+    res.render("index");
+});
+
+app.post("/get-crypto", async (req, res) => {
+    if (req.body.dropdownMenu !== "none") {
+        const userCrypto = req.body.dropdownMenu;
+
+        try {
+            console.log(API_URL + userCrypto);
+            const result = await axios.get(API_URL + userCrypto);
+            console.log(result.data);
+            console.log(typeof result.data);
+            res.render("index", { userCrypto: result.data, });
+        } catch(error) {
+            res.send(error.message);
+        }
+
+    } else {
+        const userCrypto = req.body.userCrypto;
+
+        try {
+            console.log(API_URL + userCrypto);
+            const result = await axios.get(API_URL + userCrypto);
+            console.log(result.data);
+            console.log(typeof result.data);
+            res.render("index", { userCrypto: result.data, });
+        } catch(error) {
+            res.send(error.message);
+        }
+    }
+
+
+    
+});
 
 
 app.listen(port, () => {
